002dc5fdc713d1f920eb1eeeb2585a8926d2cea3
Rotem <rotemeck123@gmail.com>
2026-06-09 13:45:08 +1000

    add multiple credentials

--- client/game.js
+++ client/game.js
@@ -6,8 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
 const RTC_CONFIG = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
     {
       urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turns:openrelay.metered.ca:443',
       username: 'openrelayproject',
       credential: 'openrelayproject'
     }
